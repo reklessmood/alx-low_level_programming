de78e82240db53c10c2014db50317acb9b618f3d
reklessmood <derioken@gmail.com>
2023-06-22 13:51:22 -0700

    more nested functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+Question #8
+What is the return value of the following function?
+
+int some_function(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+    }
+    return(i);
+}

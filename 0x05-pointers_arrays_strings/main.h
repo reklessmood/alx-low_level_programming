a179f1275f7d78186243cbea94c60dd18415b44f
reklessmood <derioken@gmail.com>
2023-06-26 13:56:26 -0700

    pointers arr

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function declarations */
+void reset_to_98(int *n);
+char *keygen(void);
+unsigned int _strlen(const char *s);
+void print_rev(char *s);
+void puts2(const char *str);
+void print_array(int *a, int n);
+int _putchar(char c);
+int _atoi(char *s);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void rev_string(char *s);
+void puts_half(char *str);
+char *_strcpy(char *dest, const char *src);
+
+#endif /* MAIN_H */

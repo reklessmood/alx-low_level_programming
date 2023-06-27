a943e8592037a03ae7b54bf8a2d4dd8796fa55bc
reklessmood <derioken@gmail.com>
2023-06-26 17:18:05 -0700

    working hard

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
-int main(void)
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates to 98
+ * @n: input
+ * Return: void
+ */
+void reset_to_98(int *n)
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return 0;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function declarations */
-void reset_to_98(int *n);
-char *keygen(void);
-unsigned int _strlen(const char *s);
-void print_rev(char *s);
-void puts2(const char *str);
-void print_array(int *a, int n);
-int _putchar(char c);
-int _atoi(char *s);
-void swap_int(int *a, int *b);
-void _puts(char *str);
-void rev_string(char *s);
-void puts_half(char *str);
-char *_strcpy(char *dest, const char *src);
+#include <stdio.h>
+#include <unistd.h>
 
-#endif /* MAIN_H */
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif

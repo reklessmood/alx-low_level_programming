b523c226b05e53f3c4264aa8036a144b795dc968
reklessmood <derioken@gmail.com>
2023-07-13 15:34:00 -0700

    coding

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,50 +1,101 @@
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer
- * @old_size: old size
- * @new_size: new size
- * Return: void
+ * is_digit - checks if a string contains only digit characters
+ * @str: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int is_digit(char *str)
 {
-	char *np, *op;
-	unsigned int i;
+	int i = 0;
 
-	if (new_size == old_size)
+	while (str[i])
 	{
-		return (ptr);
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
 	}
-	if (new_size == 0 && ptr)
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		free(ptr);
-		return (NULL);
+		i++;
 	}
-	if (!ptr)
+	return (i);
+}
+
+/**
+ * errors - handles errors for the main function
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, flag = 0;
+
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
+		errors();
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		return (malloc(new_size));
-	}
-	np = malloc(new_size);
-	if (!np)
-	{
-		return (NULL);
-	}
-	op = ptr;
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
+		digit1 = num1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
 		{
-			np[i] = op[i];
+			digit2 = num2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	else
+	for (i = 0; i < len - 1; i++)
 	{
-		for (i = 0; i < new_size; i++)
-		{
-			np[i] = op[i];
-		}
+		if (result[i])
+			flag = 1;
+		if (flag)
+			_putchar(result[i] + '0');
 	}
-	free(ptr);
-	return (np);
+	if (!flag)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }

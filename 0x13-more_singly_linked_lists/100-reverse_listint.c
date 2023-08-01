2afc07c59c9d49dcce6cea961e773904bb8f2359
reklessmood <133616162+reklessmood@users.noreply.github.com>
2023-08-01 23:38:03 +0100

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,47 +1,25 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at index index of
- * a listint_t linked list.
- * @head: A pointer to a pointer to the head of the list.
- * @index: The index of the node that should be deleted. Index starts at 0.
+ * reverse_listint - Reverses a linked list in place.
+ * @head: Double pointer to the head of the linked list.
  *
- * Return: (1) if the deletion succeeded, (-1) if it failed.
+ * Return: Pointer to the new head of the reversed list.
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *current, *temp;
-	unsigned int count = 0;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	if (head == NULL || *head == NULL)
-		return (-1);
-
-	if (index == 0)
+	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-		return (1);
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	current = *head;
-	while (current != NULL)
-	{
-		if (count == index - 1)
-		{
-			temp = current->next;
-			if (temp == NULL)
-				return (-1);
+	*head = prev;
 
-			current->next = temp->next;
-			free(temp);
-			return (1);
-		}
-
-		current = current->next;
-		count++;
-	}
-
-	return (-1); /* Unable to delete the node at index idx.*/
+	return (*head);
 }
